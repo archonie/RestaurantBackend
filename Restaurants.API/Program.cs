e66ee0dd320288fce4b03df2fe63c218a02a4204
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-02-26 23:14:39 +0300

    Add Authorization

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,47 +1,19 @@
-using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
-using Serilog.Events;
-
+ 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer"
-    });
-    
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth"  }
-            },
-            []
-        }
-    });
-});
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-
+builder.AddPresentation();
 builder.Services.AddApplication();  
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Host.UseSerilog((context, configuration) => 
-    configuration
-        .ReadFrom.Configuration(context.Configuration)  
-);
+
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();
