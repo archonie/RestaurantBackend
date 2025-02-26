eee200acbbfae72e8e7a9c443883b473e39c3610
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-02-26 22:53:56 +0300

    Add Identity

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,5 +1,6 @@
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
+using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
@@ -40,6 +41,8 @@ if (app.Environment.IsDevelopment())
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.MapIdentityApi<User>();
 
 app.UseAuthorization();
 
--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
@@ -17,6 +18,10 @@ public static class ServiceCollectionExtensions
             .UseNpgsql(connectionString)
             .EnableSensitiveDataLogging()
         );
+        
+        services.AddIdentityApiEndpoints<User>()
+            .AddEntityFrameworkStores<RestaurantDbContext>();
+        
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();   
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();   
         services.AddScoped<IDishesRepository, DishesRepository>();   
